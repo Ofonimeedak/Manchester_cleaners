67f291fa3a220991a257bf24cbd35447f4dd4bf7
RahwaZeslusHaile <rahwazeslase@gmail.com>
2026-01-12 18:52:10 +0000

    updating userRepository by adding late user cancellation penalty logic

--- Repository/userRepository.js
+++ Repository/userRepository.js
@@ -1,42 +1,69 @@
 import mongoose from "mongoose";
 import User from "../../model/user.js";
 
-class UserRepository {  
-   constructor(model = User) {
-        this.model = model;
-    }
-    async createUser(userData) {
-        const user = new this.model(userData);
-        return await user.save();
-    }
-    async findUserByEmail(email) {
-        return await this.model.findOne({ email });
-    }
+class UserRepository {
+  constructor(model = User) {
+    this.model = model;
+  }
 
-    async findUserById(id) {
-        return await this.model.findById(id);
-    }
-    async updateUserVerificationStatus(id, status) {
-        return await this.model.findByIdAndUpdate(
-            id,
-            { isVerified: status },
-            { new: true }
-        );
-    }
+  async createUser(userData) {
+    const user = new this.model(userData);
+    return await user.save();
+  }
 
-    async deleteUser(id) {
-        return await this.model.findByIdAndDelete(id);
-    }
+  async findUserByEmail(email) {
+    return await this.model.findOne({ email });
+  }
 
-async getUnverifiedUsersRaw() {
+  async findUserById(id) {
+    return await this.model.findById(id);
+  }
+
+  async updateUserVerificationStatus(id, status) {
+    return await this.model.findByIdAndUpdate(
+      id,
+      { isVerified: status },
+      { new: true }
+    );
+  }
+
+  async deleteUser(id) {
+    return await this.model.findByIdAndDelete(id);
+  }
+
+  async getUnverifiedUsersRaw() {
     return await mongoose.connection.db
-          .collection('users')
-          .find({ isVerified: false }).toArray();
+      .collection("users")
+      .find({ isVerified: false })
+      .toArray();
+  }
+
+  async bulkVerifyUsersRaw() {
+    return await mongoose.connection.db
+      .collection("users")
+      .updateMany({ isVerified: false }, { $set: { isVerified: true } });
+  }
+
+  async blockCleaner(id, durationInHours = 24) {
+    const blockedUntil = new Date(Date.now() + durationInHours * 60 * 60 * 1000);
+    return await this.model.findByIdAndUpdate(
+      id,
+      { blockedUntil },
+      { new: true }
+    );
+  }
+
+  async applyCancellationPenalty(cleanerId, bookingAmount) {
+    const penalty = bookingAmount * 0.3; 
+    const cleanerShare = bookingAmount * 0.2; 
+    const companyShare = bookingAmount * 0.1; 
+
+    return await this.model.findByIdAndUpdate(
+      cleanerId,
+      { $inc: { balance: -penalty } },
+      { new: true }
+    );
+  }
 }
 
-async bulkVerifyUserRaw() { 
-    return await mongoose.connection.db
-          .collection('users')
-          .updateMany({ isVerified: false }, { $set: { isVerified: true } });
-}
-}   
+export default UserRepository;
