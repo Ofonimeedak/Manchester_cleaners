b3c01404d9ffc2ccaf52a1322f8962f6b5f11cc1
RahwaZeslusHaile <rahwazeslase@gmail.com>
2026-01-13 20:53:13 +0000

    adding a cleaner verification logic with a dto file

--- Repository/userRepository.js
+++ Repository/userRepository.js
@@ -31,39 +31,6 @@ class UserRepository {
     return await this.model.findByIdAndDelete(id);
   }
 
-  async getUnverifiedUsersRaw() {
-    return await mongoose.connection.db
-      .collection("users")
-      .find({ isVerified: false })
-      .toArray();
-  }
-
-  async bulkVerifyUsersRaw() {
-    return await mongoose.connection.db
-      .collection("users")
-      .updateMany({ isVerified: false }, { $set: { isVerified: true } });
-  }
-
-  async blockCleaner(id, durationInHours = 24) {
-    const blockedUntil = new Date(Date.now() + durationInHours * 60 * 60 * 1000);
-    return await this.model.findByIdAndUpdate(
-      id,
-      { blockedUntil },
-      { new: true }
-    );
-  }
-
-  async applyCancellationPenalty(cleanerId, bookingAmount) {
-    const penalty = bookingAmount * 0.3; 
-    const cleanerShare = bookingAmount * 0.2; 
-    const companyShare = bookingAmount * 0.1; 
-
-    return await this.model.findByIdAndUpdate(
-      cleanerId,
-      { $inc: { balance: -penalty } },
-      { new: true }
-    );
-  }
 }
 
 export default UserRepository;
